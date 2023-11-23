b5cd75d98fbd541ab1f71aa463a7eec69c07c2ad
thierrysh <shingiro9@gmail.com>
2023-11-23 10:13:10 -0800

    Taske initial

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,24 +1,17 @@
-i#include "main.h"
+#include "main.h"
 #include <stdlib.h>
 /**
- * create_array - create array of size size and assign char c
- * @size: size of array
- * @c: char to assign
- * Description: create array of size size and assign char c
- * Return: pointer to array, NULL if fail
+ * malloc_checked - allocates memory using malloc, exit(98) if it fails
+ * @b: size of the memory block to be allocated
  *
+ * Return: pointer to the address of the memory block
  */
-char *create_array(unsigned int size, char c)
+void *malloc_checked(unsigned int b)
 {
-	char *arr;
-	unsigned int i;
+	void *block;
 
-	arr = malloc(sizeof(char) * size);
-	if (size == 0 || arr == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		arr[i] = c;
-	return (arr);
+	block = malloc(b);
+	if (block == NULL)
+		exit(98);
+	return (block);
 }
-
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+i#include "main.h"
+#include <stdlib.h>
+
+/**
+* _calloc - allocate memory using malloc and initialize it to zero
+*@member: number of elements
+*@size: size of the memory block to be allocated
+*
+*Return: poiner to the address of the memory block
+*/
+void *_calloc(unsigned int member, unsigned int size)
+{
+	char *block;
+	unsigned int i;
+
+	if (member == 0 || size == 0)
+		return (NULL);
+	block = malloc(member * size);
+	if (block != NULL)
+	{
+		for (i = 0; i < (member * size); i++)
+			block[i] = 0;
+		return (block);
+	}
+	else
+		return (NULL);
+}
+
